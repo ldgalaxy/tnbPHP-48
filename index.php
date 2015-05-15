dc1dc079b963426403843e357832d2fab12dc6eb
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-15 10:47:59 -0600

    Changes made from lesson 49

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$offset = 0
+$offset = 0;
 $find = 'is';
 $find_length = strlen($find);
 
@@ -8,7 +8,7 @@ $string = 'This is a string, and it is an example.';
 
 
 while($string_position = strpos($string, $find, $offset)){
-   
-
+   echo '<strong>' . $find . '</strong>  found at ' . $string_position . '<br>';
+   $offset = $string_position + $find_length;
 }
 ?>
